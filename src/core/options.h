f464b1352588ba8da6442d6de559ee0056e39cc6
trip5 <i@donotbend.net>
2026-07-31 04:05:09 -0300

    2026.07.31

--- src/core/options.h
+++ src/core/options.h
@@ -2,7 +2,7 @@
 #define options_h
 #pragma once
 
-#define RADIOVERSION "2026.07.20"
+#define RADIOVERSION "2026.07.31"
 
 /*******************************************************
 THIS FILE IS THE DEFINITIVE HANDLER OF COMPILE OPTIONS.
